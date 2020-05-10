b0c06319f60977e9c1f546eb2f62aac8c9bd5512
TJ Hooker <tyrelhooker@gmail.com>
2020-05-09 22:07:20 -0400

    Create initial file with create-react-app. Create form to take user input. Create JSON file with data for app development.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [myPlants, setMyPlants] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+
+    if (myPlants.includes(inputValue)) {
+      console.log('Already in array.')
+      console.log(myPlants);
+      setInputValue('');
+      const errorMessage = <p>{`The ${inputValue} is already in your plants.`}</p>
+    } else {
+        const newPlants = [...myPlants, inputValue];
+        setMyPlants(newPlants);
+        setInputValue('');
+        console.log(myPlants);
+    } 
+  
+  };
+
+  const addedPlants = myPlants.filter(plant => 
+    plant.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
+  useEffect(() => {
+   console.log(myPlants);
+  }, [myPlants]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Plants </h1>
+      
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='addPlant'>Add a Plant: </label>
+        <input value={inputValue} onChange={e => setInputValue(e.target.value)} />
+      </form>
+      <ul>
+        {addedPlants.map((plant) =>
+          <li>{plant}</li>
+        )}
+      </ul>
     </div>
   );
 }
