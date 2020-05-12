1c1c940a0b9e99eaa3902edbc7605387589709c2
TJ Hooker <tyrelhooker@gmail.com>
2020-05-12 00:09:51 -0400

    Add logic to display props from static data

--- src/components/StaticPlants.js
+++ src/components/StaticPlants.js
@@ -3,16 +3,22 @@ import React from 'react';
 
 
 const StaticPlants = ({ staticPlantData }) => {
-  const nameNormalizer = (name) => name.toLowerCase().replace(/ /g, "_"); 
+  // const nameNormalizer = (name) => name.toLowerCase().replace(/ /g, "_"); 
 
-  const savedPlants = staticPlantData.map((key) => nameNormalizer(key))
+  const savedPlants = staticPlantData;
   
   return (
     // <p>{savedPlants[1]["common name"]}</p>
     savedPlants.map((plant, i) => 
       <div key={i}>
-        <p>{plant.scientific_name}</p>
-        <p>{plant.common_name}</p>
+        <h2>{plant.scientific_name}</h2>
+        
+        <div className="common-name">
+          <h3>Common Name</h3>
+          {plant.common_name.map((com_name, j) =>
+            <p key={j}>{`${com_name}`}</p>
+          )}
+        </div>
       </div>
     )
   )
