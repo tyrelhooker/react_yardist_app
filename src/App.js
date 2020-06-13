b4551cc4dfebef474160aac0653d1a0a561376e9
TJ Hooker <tyrelhooker@gmail.com>
2020-06-13 00:52:49 -0400

    Create a hook function to use in place of useState to better sync state to local storage so data persists through page refresh.

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ const App = () => {
   // window.onunload = () => window.localStorage.clear();
 
   // const initializePlants = localStorage.getItem('storedPlants');
-  const initializeCount = JSON.parse(localStorage.getItem('storedCount')) || 0;
+  // const initializeCount = JSON.parse(localStorage.getItem('storedCount')) || 0;
 
-  const [plants, setPlants] = useState([]);
-  const [count, setCount] = useState(initializeCount);
+  const [plants, setPlants] = useLocalStorage('storedPlants', []);
+  // const [count, setCount] = useState(initializeCount);
 
-  const handleDecrement = () =>
-    setCount(currentCount => currentCount - 1);
+  // const handleDecrement = () =>
+  //   setCount(currentCount => currentCount - 1);
   
-  const handleIncrement = () =>
-    setCount(currentCount => currentCount + 1);
+  // const handleIncrement = () =>
+  //   setCount(currentCount => currentCount + 1);
   
-  useEffect(() => {
-    localStorage.setItem('storedCount', JSON.stringify(count));
-    console.log(localStorage);
-    }, [count]);
+  // useEffect(() => {
+  //   localStorage.setItem('storedCount', JSON.stringify(count));
+  //   console.log(localStorage);
+  //   }, [count]);
 
   
 
@@ -44,17 +44,17 @@ const App = () => {
 
   
 
-  const removePlant = (id) => {
-    const revisedPlants = plants.filter( plant => plant.id !== id );
-    setPlants(revisedPlants);
-  }
+  // const removePlant = (id) => {
+  //   const revisedPlants = plants.filter( plant => plant.id !== id );
+  //   setPlants(revisedPlants);
+  // }
 
-  useEffect(() => {
-    localStorage.setItem('storedPlants', plants);
-    // plants.map(plant => JSON.stringify(plant)));
-    console.log(localStorage); 
-    // setPlants(localStorage.getItem('storedPlants'))
-  }, [plants]);
+  // useEffect(() => {
+  //   localStorage.setItem('storedPlants', plants);
+  //   // plants.map(plant => JSON.stringify(plant)));
+  //   console.log(localStorage); 
+  //   // setPlants(localStorage.getItem('storedPlants'))
+  // }, [plants]);
 
 
 
@@ -67,6 +67,38 @@ const App = () => {
       <PlantsList plants={plants} onRemove={removePlant} />
     </div>
   );
+}
+
+// Hook
+function useLocalStorage(key, initialValue) {
+  // State to store value. Pass initial state function to useState so logic only used once.
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get value from local storage by it key
+      const item = window.localStorage.getItem(key);
+      // Parse stored JSON or if none return initial value
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If there is an error then return the initialValue
+      console.log(error)
+      return initialValue;
+    }
+  });
+
+  const setValue = value => {
+    try{
+      // Allow value to be a function to have same API as useState
+      const valueToStore = 
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  return [storedValue, setValue];
 }
 
 export default App;
