dc833e6d149d737e0c65df434a38adead04435d2
TJ Hooker <tyrelhooker@gmail.com>
2020-06-22 23:08:27 -0400

    Cleanup app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { v4 } from 'uuid';
 import AddPlantForm from './components/AddPlantForm';
 import PlantsList from './components/PlantsList';
-// import StaticPlants from './components/StaticPlants';
-// import Count from './components/Count';
-
 import './stylesheets/App.scss';
 
 function useLocalStorage(key, initialValue) {
@@ -44,20 +41,6 @@ function useLocalStorage(key, initialValue) {
 const App = () => {
   // Plants
   const [plants, setPlants] = useLocalStorage('storedPlants', []);
-  // const [count, setCount] = useState(initializeCount);
-
-  // const handleDecrement = () =>
-  //   setCount(currentCount => currentCount - 1);
-  
-  // const handleIncrement = () =>
-  //   setCount(currentCount => currentCount + 1);
-  
-  // useEffect(() => {
-  //   localStorage.setItem('storedCount', JSON.stringify(count));
-  //   console.log(localStorage);
-  //   }, [count]);
-
-  
 
   const addPlant = (scientificName, commonName) => {
     const newPlants = [
@@ -71,20 +54,16 @@ const App = () => {
     setPlants(newPlants);
   }
 
-  
-
   const removePlant = (id) => {
     const revisedPlants = plants.filter( plant => plant.id !== id );
     setPlants(revisedPlants);
   }
 
-  useEffect(() => window.localStorage.setItem('storagePlants', JSON.stringify(plants)), [plants]);
+  // useEffect(() => window.localStorage.setItem('storagePlants', JSON.stringify(plants)), [plants]);
 
 
   return (
     <div className="App">
-      {/* {console.log({plants})} */}
-      {/* <Count testCount={count} onDecrement={handleDecrement} onIncrement={handleIncrement}/> */}
       <AddPlantForm onNewPlant={addPlant} />
       <PlantsList plants={plants} onRemove={removePlant} />
     </div>
